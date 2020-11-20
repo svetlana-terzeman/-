17d0bcd4a02cf5f4d8ca8808b38159b71d1ea83d
svetlana-terzeman <74791110+svetlana-terzeman@users.noreply.github.com>
2020-11-20 22:12:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Введтите своё имя ");
+            string name = ReadLine();
+            WriteLine($"Hello, {name}!");
+            
+        }
+    }
+}
